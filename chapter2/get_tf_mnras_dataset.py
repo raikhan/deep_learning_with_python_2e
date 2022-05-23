8c091aaed18466daf6ce48ecf3aed86263bee795
Milan Raicevic <milanr@rmt.com.au>
2022-05-23 16:59:01 +0800

    code to export MNRAS from Keras

---
+++ chapter2/get_tf_mnras_dataset.py
@@ -0,0 +1,17 @@
+#
+# Load the data from Keras to use in the PyTorch version of Chapter 2
+#
+from tensorflow.keras.datasets import mnist
+
+import joblib
+
+print("Exporting MNRAS dataset from Keras")
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+data = {
+    "images": {"train": train_images, "test": test_images},
+    "labels": {"train": train_labels, "test": test_labels},
+}
+
+joblib.dump(data, "mnras_keras_data.joblib")
+print("Done")
