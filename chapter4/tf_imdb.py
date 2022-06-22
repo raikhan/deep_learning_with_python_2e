597d89397a04c045eccfd934cca0f1ba2b4a8212
Milan Raicevic <milanr@rmt.com.au>
2022-06-22 13:57:38 +0800

    continued on IMDB example

--- chapter4/tf_imdb.py
+++ chapter4/tf_imdb.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers
 from tensorflow.keras.datasets import imdb
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
@@ -35,4 +36,28 @@ x_test = multi_hot_encode(test_data)
 y_train = np.asarray(train_labels).astype("float32")
 y_test = np.asarray(test_labels).astype("float32")
 
-# Continue 4.1.3
+# add a validation set from training set
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+# Defining the model
+model = keras.Sequential(
+    [
+        layers.Dense(16, activation="relu"),
+        layers.Dense(16, activation="relu"),
+        layers.Dense(1, activation="sigmoid"),
+    ]
+)
+model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
+
+
+# fit the model using validation set for monitoring progress
+history = model.fit(
+    partial_x_train,
+    partial_y_train,
+    epochs=20,
+    batch_size=512,
+    validation_data=(x_val, y_val),
+)
